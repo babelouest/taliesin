47b5998d6a28918f1943eea8e42baa958e48ab6e
Nicolas Mora <github@babelouest.org>
2018-03-10 08:35:42 -0500

    Upgrade minor version

--- src/taliesin.h
+++ src/taliesin.h
@@ -24,7 +24,7 @@
 #ifndef __TALIESIN_H_
 #define __TALIESIN_H_
 
-#define _TALIESIN_VERSION_ "1.0.12"
+#define _TALIESIN_VERSION_ "1.0.13"
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))

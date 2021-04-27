42fb127f829db20c80a2a7c83b3284093c74ce44
Nicolas Mora <github@babelouest.org>
2021-04-27 19:40:14 -0400

    Improve webpack dev config

--- webapp-src/webpack.dev.config.js
+++ webapp-src/webpack.dev.config.js
@@ -1,38 +1,50 @@
 var path = require('path');
 
 module.exports = {
-		entry: './src/index.js',
-		devtool: 'inline-source-map',
-		output: {
-				path: path.resolve(__dirname),
-				filename: 'bundle.js',
-				libraryTarget: 'umd'
-		},
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js',
+    libraryTarget: 'umd'
+  },
 
-		devServer: {
-				contentBase: path.resolve(__dirname),
-				compress: true,
-				port: 3000,
-				host: 'localhost',
-				open: true
-		},
+	devServer: {
+		contentBase: path.resolve(__dirname),
+		compress: true,
+		port: 3000,
+		host: 'localhost',
+		open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8576/',
+        secure: false,
+        changeOrigin: true
+      },
+      '/config': {
+        target: 'http://localhost:8576/',
+        secure: false,
+        changeOrigin: true
+      }
+    }
+	},
 
-		module: {
-				loaders: [
-						{
-								test: /\.js$/,
-								exclude: /(node_modules|bower_components|build)/,
-								use: {
-										loader: 'babel-loader',
-										options: {
-												presets: ['env']
-										}
-								}
-						},
-						{
-								test: /\.css$/,
-								loader: 'style-loader!css-loader'
-						}
-				]
-		}
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
 }
